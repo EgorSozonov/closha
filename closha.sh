6625fad6e1f730506e1c2e3f6141c5fad3997730
EgorSozonov <Egor.Sozonov.09@gmail.com>
2026-08-10 10:49:34 +0300

    Implementing restoration from backup

--- closha.sh
+++ closha.sh
@@ -3,16 +3,27 @@
 shopt -s extglob
 shopt -s nullglob
 
+### ### ### ### script state ### ### ### ### ### ###
 declare -a configFiles
+#The "output"s from a config file
 declare -a bupDirs
+
+#The "input"s from a config file
 declare -a inputs
+#The pairs of "inputCommand/inputResult" from a config file
 declare -a inputCommands
 declare -a inputResults
+#How many versions of a backup to keep
 declare -i keepVersions
+### ### ### ### ### #### ### ### ### ### ### ### ###
 
 #$1 = config file
 function readConfig() {
    local state="default" # default | inputCommand
+   bupDirs=()
+   inputs=()
+   inputCommands=()
+   inputResults=()
    
    while IFS=" = " read k v; do
       if [[ "$v" == "" ]]; then
@@ -57,7 +68,7 @@ function readConfig() {
       exit 1
    fi
    if (("${#inputCommands[@]}" != "${#inputResults[@]}")) then
-      echo "The number of commands should match number of outputs" # Should be unreachable
+      echo "The number of commands should match number of outputs" #Should be unreachable
       exit 1
    fi
    if [[ $state == "inputCommand" ]]; then
@@ -67,27 +78,21 @@ function readConfig() {
 }
 
 #$1 = short file name without the ".123.bak" suffix. $2 = output dir. 
-#$3 = OUT min backup id, $4 = OUT max backup id
-function getEarliestAndLatestVersions() {
-   local -n minId=$3
+#$3 = OUT array of existing ids, $4 = OUT max backup id
+function getAllAndLatestVersions() {
+   local -n existing=$3
    local -n maxId=$4
-   echo ""
-   echo "Getting ids from $2/$1.*.bak"
    
-   #declare -a existingBackups
-   #readarray -t existingBackups < <(ls -A "$2/$1.*.bak" 2>/dev/null)
-   existingBackups=( $2/$1.*.bak )
+   local existingBackups=( $2/$1.*.bak )
    
-   echo "got ${#existingBackups[@]} existing backups"
    ((maxId = -1))
    
    for fn in "${existingBackups[@]}"; do
       if [[ "$fn" =~ $2/$1.([0-9]+).bak ]]; then
          local bId=$(( "${BASH_REMATCH[1]}" ))
-         echo "encountered backup $bid"
-         if (( bId < minId )) then
-            ((minId = bId))
-         elif (( bId > maxId )) then  
+         existing+=(bId)
+         
+         if (( bId > maxId )) then  
             ((maxId = bId))
          fi
       fi
@@ -110,44 +115,42 @@ function checkIfFilesDiffer() {
    ((out = 0))
 }
 
-#$1 user command. $2 inpResult $3 inpShort $4 out
+#$1 inpResult $2 inpShort $3 output
 function makeABackup() {
-   declare -i minBId
-   declare -i maxBId
-   
-   $1 #Run user command to (hopefully) create the inpRes file 
-   
-   if ! [[ -f "$2" ]]; then
+   if ! [[ -f "$1" ]]; then
       echo "Expected a file to back up but can't find it!"
-      echo "|| $2 ||"
+      echo "|| $1 ||"
       exit 1
    fi
    
-   getEarliestAndLatestVersions $3 $4 minBId maxBId
+   declare -i maxBId
+   declare -a existingVersions
+   getAllAndLatestVersions $2 $3 existingVersions maxBId
    local newId=$(( maxBId + 1 ))
-   echo "minId $minId max $maxId newId $newId"
+   echo "max $maxBId newId $newId"
    
-   declare -i filesDiffer
-   checkIfFilesDiffer $2 "$4/$3.$maxId.bak" filesDiffer
-   if (( filesDiffer == 0)) then
-      return 0
+   if (( maxBId >= 0)) then
+      declare -i filesDiffer
+      checkIfFilesDiffer $1 "$3/$2.$maxBId.bak" filesDiffer
+      if (( filesDiffer == 0)) then
+         return 0
+      fi
    fi
    
-   local newName="$4/$inpShort.$newId.bak"
+   local newName="$3/$inpShort.$newId.bak"
    local newChecksumName="$output/$inpShort.$newId.sha256"
    cp $inpRes $newName
    
-   origChecksumAndFname="$(sha256sum $2)"
+   origChecksumAndFname="$(sha256sum $1)"
    newChecksumAndFname="$(sha256sum $newName)"
    origChecksum="${origChecksumAndFname:0:64}"
    newChecksum="${newChecksumAndFname:0:64}"
    if [[ "$newChecksum" != "$origChecksum" ]]; then
       echo "Error when copying file "
-      echo "$2"
-      echo "to $4"
-      echo "Orig checksum $origChecksum, new checksum $newChacksum"
+      echo "$1"
+      echo "to $3"
    fi
-   echo "$newChecksumAndFname" > "$newChecksumName"
+   echo "$newChecksum" > "$newChecksumName"
 }
 
 function makeBackups() {
@@ -174,32 +177,83 @@ function makeBackups() {
       echo "$x"
    done
    
+   #Ordinary inputs
+   for ((i=0; i< "${#inputs[@]}"; i++)) do
+      local input="${inputs[i]}"
+      local inpShort="${input##*/}"
+      
+      for output in "${bupDirs[@]}"; do
+         makeABackup $input $inpShort $output
+      done
+   done
    
+   #Inputs generated from commands
    for ((i=0; i< "${#inputCommands[@]}"; i++)) do
       local comm="${inputCommands[i]}"
       local inpRes="${inputResults[i]}"
       local inpShort="${inpRes##*/}"
       
+      $1 #Run user command to (hopefully) create the inpRes file 
+      
       for output in "${bupDirs[@]}"; do
-         makeABackup $comm $inpRes $inpShort $output
+         makeABackup $inpRes $inpShort $output
       done
+      
+      if [[ -f "$inpRes" ]]; then
+         /usr/bin/rm "$inpRes"
+      fi
    done
 }
 
-# $1 = backup file name
+#If the restored backup is an archive, unpacks it to a subdir and deletes the archive
+#$1 = backup file name
+function tryToUnpack() {
+}
+
+#$1 = backup file name. $2 = restoration path
 function restoreFromBackup() {
 # get checksum from  file
 # calculate checksum from $1
 # if not same, display error
-   echo "restoring"
-}
-
-function main() {
-   if [[ "$1" == "" ]]; then
-      makeBackups
+   echo "restoring $1"
+   local bup="$1"
+   if ! [[ -f $bup ]]; then
+      echo "File doesn't exist or is a directory!"
+      exit 1
+   elif [[ "$bup" =~ .*\.([0-9]+).bak ]]; then
+      local bId=$(( "${BASH_REMATCH[1]}" ))
+      local checksumFname="${bup%.bak}.sha256"
+      
+      if ! [[ -f "$checksumFname" ]]; then
+         echo "Checksum file doesn't exist, cannot validate the backup!"
+         exit 1
+      fi
+      
+      local restoredChecksumAndFname="$(sha256sum $1)"
+      local restoredChecksum="${restoredChecksumAndFname:0:64}"
+      local readChecksum=$(<$checksumFname)
+      if [[ "$restoredChecksum" != "$readChecksum" ]]; then
+         echo "Bitrot detected in backup! Current checksum is"
+         echo "$restoredChecksum"
+         echo "but the one written upon creation is"
+         echo "$readChecksum"
+         exit 1
+      fi
+      
+      local restoredFname="${bup%.$bId.bak}"
+      /usr/bin/cp "$bup" "$restoredFname"
    else
-      restoreFromBackup $1
+      echo "The file name doesn't check out. It should end with a number like '.123.bak'"
+      exit 1
    fi
 }
 
-main
+
+#Main script
+if [[ "$1" == "" ]]; then
+   makeBackups
+else
+   restorePath="~/.local/share/closha/"
+   mkdir -p "$restorePath"
+   restoreFromBackup $1 $restorePath
+fi
